827122705d51b9fb70532ba1430fbdff32c9be99
Mark Dolinar <S-Mark.Dolinar@Lwtech.Edu>
2022-03-26 13:42:25 -0700

    Initial commit

---
+++ src/com/codingagain/Main.java
@@ -0,0 +1,23 @@
+package com.codingagain;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Principle:");
+        int principle = scanner.nextInt();
+        System.out.println("Annual Interest Rate:");
+        float annualInterestRate = scanner.nextFloat();
+        System.out.println("Period(Years):");
+        int period = scanner.nextInt();
+    }
+
+    // Principle:
+    // Annual Interest Rate:
+    // Period (Years):
+    // Mortgage:
+
+
+}
