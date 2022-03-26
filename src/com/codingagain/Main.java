84eea3ec695f2a57e06150226fbe097b4b497bf2
Mark Dolinar <S-Mark.Dolinar@Lwtech.Edu>
2022-03-26 14:11:17 -0700

    Completed Mortgage Calculator

--- src/com/codingagain/Main.java
+++ src/com/codingagain/Main.java
@@ -1,23 +1,34 @@
 package com.codingagain;
 
+import java.text.NumberFormat;
+import java.util.Currency;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Principle:");
+
+        final byte monthsInYear = 12;
+        final byte percentage = 100;
+
+        System.out.print("Principle:");
         int principle = scanner.nextInt();
-        System.out.println("Annual Interest Rate:");
+
+        System.out.print("Annual Interest Rate:");
         float annualInterestRate = scanner.nextFloat();
-        System.out.println("Period(Years):");
+        float monthlyInterestRate = annualInterestRate/monthsInYear/percentage;
+
+        System.out.print("Period(Years):");
         int period = scanner.nextInt();
+        int totalPayments = period * 12;
+
+        double mortgage = principle
+                * ((monthlyInterestRate * (Math.pow((1 + monthlyInterestRate), totalPayments)))
+                /((Math.pow((1 + monthlyInterestRate), totalPayments)) -1));
+
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.print("Mortgage " + mortgageFormatted);
     }
-
-    // Principle:
-    // Annual Interest Rate:
-    // Period (Years):
-    // Mortgage:
-
 
 }
